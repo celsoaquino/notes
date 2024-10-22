094413434ece99b02e075f8aed20672d7c00c3d3
celsoaquino <celso.aquino@ymail.com>
2024-10-22 10:23:50 -0300

    Created login and logout

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIfLogged;
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware([CheckIsNotLogged::class])->group(function () {
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/auth', [AuthController::class, 'auth']);
 });
+
+Route::middleware([CheckIfLogged::class])->group(function () {
+    Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+});
+
+
